f011f0862a379db880d5f9bec914e2bda955ba56
Roy Wakumelo Jr <roywakumelo@roys-mbp.attlocal.net>
2019-11-20 19:32:06 -0500

    Date update

--- database/seeds/002-tasks.js
+++ database/seeds/002-tasks.js
@@ -8,39 +8,39 @@ exports.seed = function(knex) {
       user_id: '1',
       title: 'Create a marketing page',
       completed: 'false',
-      created_at: moment().format('MM-DD-YYYY'),
+      created_at: moment().format('MMMM-Do-YYYY'),
     }, 
 
     {
       user_id: '1',
       title: 'Create a react app', 
       completed: 'false', 
-      created_at: moment().format('MM-DD-YYYY'),
+      created_at: moment().format('MMMM-Do-YYYY'),
     }, 
     {
       user_id: '1',
       title: 'Create a Node Api', 
       completed: 'false', 
-      created_at: moment().format('MM-DD-YYYY'),
+      created_at: moment().format('MMMM-Do-YYYY'),
     },
     {
       user_id: '2',
       title: 'Create a marketing page',
       completed: 'true',
-      created_at: moment().format('MM-DD-YYYY'),
+      created_at: moment().format('MMMM-Do-YYYY'),
     }, 
 
     {
       user_id: '2',
       title: 'Create a react app', 
       completed: 'true', 
-      created_at: moment().format('MM-DD-YYYY'),
+      created_at: moment().format('MMMM-Do-YYYY'),
     }, 
     {
       user_id: '3',
       title: 'Create a Node Api', 
       completed: 'true', 
-      created_at: moment().format('MM-DD-YYYY'),
+      created_at: moment().format('MMMM-Do-YYYY'),
     },
   ]);
     
